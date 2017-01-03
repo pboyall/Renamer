87c396ded57b5cccd475609b2f6fb95ce89920fd
Peter Boyall <Peter.Boyall@28b.co.uk>
2017-01-03 18:19:39 +0000

    Add project files.

---
+++ Renamer/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNames = new System.Windows.Forms.TextBox();
+            this.btnNames = new System.Windows.Forms.Button();
+            this.btnFolders = new System.Windows.Forms.Button();
+            this.txtFolders = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(831, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNames
+            // 
+            this.txtNames.Location = new System.Drawing.Point(12, 12);
+            this.txtNames.Name = "txtNames";
+            this.txtNames.Size = new System.Drawing.Size(253, 20);
+            this.txtNames.TabIndex = 1;
+            // 
+            // btnNames
+            // 
+            this.btnNames.Location = new System.Drawing.Point(287, 13);
+            this.btnNames.Name = "btnNames";
+            this.btnNames.Size = new System.Drawing.Size(75, 23);
+            this.btnNames.TabIndex = 2;
+            this.btnNames.Text = "Names";
+            this.btnNames.UseVisualStyleBackColor = true;
+            this.btnNames.Click += new System.EventHandler(this.btnNames_Click);
+            // 
+            // btnFolders
+            // 
+            this.btnFolders.Location = new System.Drawing.Point(287, 39);
+            this.btnFolders.Name = "btnFolders";
+            this.btnFolders.Size = new System.Drawing.Size(75, 23);
+            this.btnFolders.TabIndex = 4;
+            this.btnFolders.Text = "Folders";
+            this.btnFolders.UseVisualStyleBackColor = true;
+            this.btnFolders.Click += new System.EventHandler(this.btnFolders_Click);
+            // 
+            // txtFolders
+            // 
+            this.txtFolders.Location = new System.Drawing.Point(12, 38);
+            this.txtFolders.Name = "txtFolders";
+            this.txtFolders.Size = new System.Drawing.Size(253, 20);
+            this.txtFolders.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 261);
+            this.Controls.Add(this.btnFolders);
+            this.Controls.Add(this.txtFolders);
+            this.Controls.Add(this.btnNames);
+            this.Controls.Add(this.txtNames);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNames;
+        private System.Windows.Forms.Button btnNames;
+        private System.Windows.Forms.Button btnFolders;
+        private System.Windows.Forms.TextBox txtFolders;
+    }
+}
+
---
+++ Renamer/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using SpreadsheetLight;
+
+
+namespace Renamer
+{
+    public partial class Form1 : Form
+    {
+
+        public string OriginalNameCol = "A";
+        public string NewNameCol = "B";
+        public int startRow = 1;
+        Dictionary<string, string> namemap;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadLookups();
+            replaceNamedFiles();
+            replaceRecursedFiles();
+
+        }
+
+        private bool loadLookups()
+        {
+            SLDocument pubform = new SLDocument(txtNames.Text);
+            SLWorksheetStatistics stats1 = pubform.GetWorksheetStatistics();
+            namemap = new Dictionary<string, string>();
+            for (int j = 0; j <= stats1.EndRowIndex; j++)
+            {
+                string originalname = pubform.GetCellValueAsString(OriginalNameCol + j);
+                string newname = pubform.GetCellValueAsString(OriginalNameCol + j);
+                namemap.Add(originalname, newname);
+            }
+            return true;
+        }
+
+        private bool loadFiles()
+        {
+            string[] htmlfiles = Directory.GetFiles(txtFolders.Text, "*.html", SearchOption.AllDirectories);
+            string[] jsfiles = Directory.GetFiles(txtFolders.Text, "*.js", SearchOption.AllDirectories);
+            return true;
+        }
+
+        private bool replaceNamedFiles()
+        {
+            //For every file in the folder, open it and resave it with its new name
+
+            foreach (var file in namemap)
+            {
+                string[] namedfiles = Directory.GetFiles(txtFolders.Text, file.Key, SearchOption.AllDirectories); 
+                foreach(var thefile in namedfiles) { 
+                    using (var input = File.OpenText(thefile))
+                    using (var output = new StreamWriter(file.Value)) 
+                    //While we are here take the opportunity to replace every occurance of every renamed file in these named files
+                    {
+                        string line;
+                        while (null != (line = input.ReadLine()))
+                        {
+                            foreach(var oldValue in namemap) {
+                                line.Replace(oldValue.Key, oldValue.Value);
+                            }
+                            output.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool replaceRecursedFiles(string[] filearray)
+        {
+            foreach (var thefile in filearray)
+            {
+                //Grab new file name
+                    string newfile = "";
+                    namemap.TryGetValue(thefile, out newfile);
+                    using (var input = File.OpenText(thefile))
+                    using (var output = new StreamWriter(newfile))
+                    //While we are here take the opportunity to replace every occurance of every renamed file
+                    {
+                        string line;
+                        while (null != (line = input.ReadLine()))
+                        {
+                            foreach (var oldValue in namemap)
+                            {
+                                line.Replace(oldValue.Key, oldValue.Value);
+                            }
+                            output.WriteLine(line);
+                        }
+                    }
+                }
+            return true;
+        }
+
+
+
+        private void btnNames_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Rename File";
+            theDialog.Filter = "XLSX files|*.xlsx";
+            theDialog.InitialDirectory = txtFolders.Text;
+            DialogResult result = theDialog.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                txtNames.Text = theDialog.FileName.ToString(); 
+            }
+        }
+
+        private void btnFolders_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFolders.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+        }
+    }
+}
